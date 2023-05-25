769378c327efcbe5b0c1e9455da704dbe9ff1953
Santosh Sah <sah.santosh@gmail.com>
2023-05-25 09:43:40 +0545

    Added res.header to allow netlify url

--- server.js
+++ server.js
@@ -18,6 +18,18 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.use(function (req, res, next) {
+    res.header(
+        "Access-Control-Allow-Origin",
+        "https://warm-lamington-7817e5.netlify.app/"
+    ); // update to match the domain you will make the request from
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
 app.use(express.json());
 
 // parse requests of content-type - application/json
